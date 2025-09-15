c614b04cdd2dff12a0619044c24c39c7918a72e0
Sparsh5001 <sparshbro@gmail.com>
2025-09-15 19:44:08 +0530

    Time: 8 ms (45.39%) | Memory: 40.3 MB (81.51%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,17 +1,8 @@
 class Solution {
     public int fib(int n) {
-        return fibtry(n);
-    }
-
-
-    int fibtry(int n){
         if(n<2){
             return n;
         }
-        return fibtry(n-1) + fibtry(n-2);
-        
-
+        return fib(n-1) + fib(n-2);
     }
-
-
 }
