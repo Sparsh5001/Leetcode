c3371740c39704e276b8726f8e7fb473b57dff95
Sparsh5001 <sparshbro@gmail.com>
2026-04-12 14:44:45 +0530

    Time: 1 ms (88.68%) | Memory: 43.9 MB (89.85%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,39 +1,19 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        int i = 0;
-        List<Integer> temp = new ArrayList<>();
-
-        
-
-
-        return subfind (nums , i , temp , result);
+        List<Integer> cur = new ArrayList<>();
+        find(result , cur , 0 , nums);
+        return result;
     }
 
-
-    public List<List<Integer>> subfind(int[] nums , int i , List<Integer> temp , List<List<Integer>> result ){
-            
-            if(i == nums.length-1){
-                temp.add(nums[i]);
-                result.add(new ArrayList<>(temp));
-                if (temp.size() > 0) temp.remove(temp.size() - 1);
-            }
-
-            else{
-            temp.add(nums[i]);
-            subfind(nums , i+1 , temp , result);
-            }
-
-            if(i == nums.length-1){
-                result.add(new ArrayList<>(temp));
-                if (temp.size() > 0) temp.remove(temp.size() - 1);
-            }
-
-            else{
-                subfind(nums , i+1 , temp , result);
-            }
-    return result;
+    public void find(List<List<Integer>> result , List<Integer> cur , int index , int[] nums){
+        if(index == nums.length){
+            result.add(new ArrayList<>(cur));
+            return;
+        }
+        cur.add(nums[index]);              
+        find(result, cur, index + 1, nums); 
+        cur.remove(cur.size() - 1);   
+        find(result, cur, index + 1, nums); 
     }
-
-
 }
