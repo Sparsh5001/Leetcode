a9632b58a84e912ab415dffa058b5fc9eeaa0cad
Sparsh5001 <sparshbro@gmail.com>
2026-06-13 22:26:07 +0530

    Time: 15 ms (57.45%) | Memory: 48.1 MB (83.24%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.java
+++ 503-next-greater-element-ii/next-greater-element-ii.java
@@ -1,35 +1,30 @@
 class Solution {
     public int[] nextGreaterElements(int[] nums) {
         
+        /*for(int i = nums.length-1 ; i > -1 ; i--){
+            stack.push(nums[i]);
+        }*/
+        int n = nums.length;
+        int[] ans = new int[n];
+
         Stack<Integer> stack = new Stack<>();
 
-        int[] arr = new int[nums.length];
+        for (int i = 2 * n - 1; i >= 0; i--) {
 
-        for(int i = nums.length-1 ; i > -1 ; i--){
-            stack.push(nums[i]);
+            int curr = nums[i % n];
+
+            while (!stack.isEmpty() && stack.peek() <= curr) {
+                stack.pop();
+            }
+
+            if (i < n) {
+                ans[i] = stack.isEmpty() ? -1 : stack.peek();
+            }
+
+            stack.push(curr);
         }
 
-
-        for(int i = nums.length-1 ; i > -1 ; i--){
-            while(!stack.isEmpty()){
-                if(stack.peek() > nums[i]){
-                    arr[i] = stack.peek();
-                    stack.push(nums[i]);
-                    break;
-                }
-                else{
-                    stack.pop();
-                }
-            }
-            if(stack.isEmpty()){
-                arr[i] = -1;
-                stack.push(nums[i]);
-            }
-        }
-
-
-
-        return arr;
+        return ans;
 
     }
 }
