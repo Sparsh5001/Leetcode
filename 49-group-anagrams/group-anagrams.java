c57e10c487ca134b423ebf32923ff81fc3f5e44e
Sparsh5001 <sparshbro@gmail.com>
2026-06-17 15:08:20 +0530

    Time: 7 ms (73.46%) | Memory: 49.8 MB (35.67%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -2,11 +2,9 @@ class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         
         List<List<String>> ans = new ArrayList<>();
-        List<String> temp = new ArrayList<>();
         HashMap<String , List<String> > map = new HashMap<>();
 
         for(int i = 0 ; i<strs.length ; i++){
-
             char[] s = strs[i].toCharArray();
             Arrays.sort(s);
             String hold = new String(s);
@@ -14,22 +12,17 @@ class Solution {
                 map.get(hold).add(strs[i]);
             }
             else{
-                map.put(hold , new ArrayList());
+                map.put(hold , new ArrayList<>());
                 map.get(hold).add(strs[i]);
             }
         }
-
         for(Map.Entry<String,List<String>> entry:map.entrySet()){
             ans.add(entry.getValue());
         }
-
-
-
         if (ans.size()<1){
-            ans.add(new ArrayList());
+            ans.add(new ArrayList<>());
             ans.get(0).add("");
         } 
-
         return ans;
     }
 }
