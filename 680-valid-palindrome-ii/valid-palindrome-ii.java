9565a61a8f33c9b7096de7b379db0ae9f4de0fe9
Sparsh5001 <sparshbro@gmail.com>
2026-06-27 00:07:01 +0530

    Time: 4 ms (99.74%) | Memory: 48 MB (11.64%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.java
+++ 680-valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,12 +1,9 @@
 class Solution {
     public boolean validPalindrome(String s) {
-
         int i = 0 ; 
         int j = s.length()-1 ;
         while(i<j){
-            int char_i = s.charAt(i);
-            int char_j = s.charAt(j);
-            if(char_i != char_j){
+            if(s.charAt(i) != s.charAt(j)){
                return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1) ;
             }
             i++;
