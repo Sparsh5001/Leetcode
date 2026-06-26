ce9ffbac266e17deef0241c00ae1b6b1ea315aa4
Sparsh5001 <sparshbro@gmail.com>
2026-06-26 23:35:20 +0530

    Time: 2 ms (99.23%) | Memory: 44.3 MB (65.83%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+
+        int i = 0 ; 
+        int j = s.length()-1 ;
+
+        while(i<j){
+            int char_i = Character.toLowerCase(s.charAt(i));
+            int char_j = Character.toLowerCase(s.charAt(j));
+            if((char_i < 48 || char_i > 57) && (char_i < 97 || char_i > 122)){
+                i++;
+                continue;
+            }
+            if((char_j < 48 || char_j > 57) && (char_j < 97 || char_j > 122)){
+                j--;
+                continue;
+            }
+            if(char_i != char_j){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
