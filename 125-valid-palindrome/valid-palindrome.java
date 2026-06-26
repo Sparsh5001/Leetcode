a839ee30be0e8fae803e390974271181900b8759
Sparsh5001 <sparshbro@gmail.com>
2026-06-26 23:35:54 +0530

    Time: 2 ms (99.23%) | Memory: 44.1 MB (84.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        
-
         int i = 0 ; 
         int j = s.length()-1 ;
-
         while(i<j){
             int char_i = Character.toLowerCase(s.charAt(i));
             int char_j = Character.toLowerCase(s.charAt(j));
