b8c4b5c67464363a58d5ab3f63b000e5a72b5639
Sparsh5001 <sparshbro@gmail.com>
2026-06-26 23:37:02 +0530

    Time: 3 ms (60.61%) | Memory: 44.1 MB (84.24%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -5,11 +5,11 @@ class Solution {
         while(i<j){
             int char_i = Character.toLowerCase(s.charAt(i));
             int char_j = Character.toLowerCase(s.charAt(j));
-            if((char_i < 48 || char_i > 57) && (char_i < 97 || char_i > 122)){
+            if(!Character.isLetterOrDigit(char_i)){
                 i++;
                 continue;
             }
-            if((char_j < 48 || char_j > 57) && (char_j < 97 || char_j > 122)){
+            if(!Character.isLetterOrDigit(char_j)){
                 j--;
                 continue;
             }
