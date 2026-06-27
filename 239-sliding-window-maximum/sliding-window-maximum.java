8eaba319cef997f7ca72fe5fd24fc392d8203c84
Sparsh5001 <sparshbro@gmail.com>
2026-06-28 01:37:50 +0530

    Time: 28 ms (90.11%) | Memory: 147.9 MB (46.08%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,14 +1,9 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int[] ans = new int[nums.length-k+1];
-
         Deque<Integer> dq = new ArrayDeque<>();
-
         int i = 0;
         int j = 0;
-        int max = Integer.MIN_VALUE;
-        dq.offerLast(max);
-
         while(j<k){
             if(!dq.isEmpty() && nums[j]>dq.peekLast()){
                 dq.pollLast();
