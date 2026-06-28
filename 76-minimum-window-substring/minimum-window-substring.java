9c7d780ba3e03c0252fbb591a9d20833c3cf28b3
Sparsh5001 <sparshbro@gmail.com>
2026-06-29 01:40:46 +0530

    Time: 16 ms (58.36%) | Memory: 47.2 MB (48.61%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -5,47 +5,42 @@ class Solution {
         for(int i = 0 ; i <t.length() ; i++){
             map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
-
         int i = 0;
         int j = 0;
-
         int start = 0;
         int end = 0;
-
         int count = map.size();
-
         int min = Integer.MAX_VALUE;
 
         while(j<s.length()){
-
             char c = s.charAt(j);
-            if(!map.containsKey(c)){
-                j++;
-            }
-            else{
+
+            if(map.containsKey(c)){
                 map.put(c,map.get(c)-1);
                 if(map.get(c)==0){
                     count--;
                 }
-                if(count == 0 ){
-                    while (i <= j && count == 0) {
-                        if (min > j - i + 1) {
-                            min = j - i + 1;
-                            start = i;
-                            end = j + 1;
-                        }
-                        char cc = s.charAt(i);
-                        if (map.containsKey(cc)) {
-                            map.put(cc, map.get(cc) + 1);
-                            if (map.get(cc) > 0) {
+            } 
+
+            if(count == 0 ){
+                while (i <= j && count == 0) {
+                    if (min > j - i + 1) {
+                        min = j - i + 1;
+                        start = i;
+                        end = j + 1;
+                    }
+                    char cc = s.charAt(i);
+                    if (map.containsKey(cc)){
+                        map.put(cc, map.get(cc) + 1);
+                            if (map.get(cc) == 1) {
                                 count++;
                             }
-                        }
-                        i++;
                     }
+                    i++;
                 }
-                j++;
-            } 
+            }
+
+            j++;
         }
     return s.substring(start,end);
     }
