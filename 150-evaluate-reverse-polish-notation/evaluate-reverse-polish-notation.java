6bf34201c32045a653264a9f136afd5711c8473e
Sparsh5001 <sparshbro@gmail.com>
2026-07-02 01:02:39 +0530

    Time: 8 ms (11.44%) | Memory: 45.1 MB (89.90%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -2,10 +2,6 @@ class Solution {
     public int evalRPN(String[] tokens) {
         
         Stack<Integer> st = new Stack<>();
-        int ans = 0 ;
-        if(tokens.length==0){
-            return 0;
-        }
         for(String s : tokens){
             if(s.equals("*")){
                 int val1 = st.pop();
