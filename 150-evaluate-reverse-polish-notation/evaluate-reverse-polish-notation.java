d5285fb28071e2c234f95b766aa6dba01d1cde6f
Sparsh5001 <sparshbro@gmail.com>
2026-07-02 01:02:09 +0530

    Time: 7 ms (36.31%) | Memory: 45.3 MB (46.26%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,40 +1,38 @@
 class Solution {
     public int evalRPN(String[] tokens) {
         
-        Stack<String> st = new Stack<>();
+        Stack<Integer> st = new Stack<>();
         int ans = 0 ;
         if(tokens.length==0){
             return 0;
         }
-        if(tokens.length==1){
-            return Integer.parseInt(tokens[0]);
-        }
-
         for(String s : tokens){
             if(s.equals("*")){
-                ans= Integer.parseInt(st.pop()) * Integer.parseInt(st.pop());
-                st.push(Integer.toString(ans));
+                int val1 = st.pop();
+                int val2 = st.pop();
+                st.push(val1*val2);
             }
             else if(s.equals("+")){
-                ans= Integer.parseInt(st.pop()) + Integer.parseInt(st.pop());
-                st.push(Integer.toString(ans));
+                int val1 = st.pop();
+                int val2 = st.pop();
+                st.push(val1+val2);
             }
-            else if(s.equals("-")){
-                ans= (Integer.parseInt(st.pop()) - Integer.parseInt(st.pop()))*-1;
-                st.push(Integer.toString(ans));
+            else if(s.equals("-")){ 
+                int val1 = st.pop();
+                int val2 = st.pop();
+                st.push(val2-val1);
             }
             else if(s.equals("/")){
-                int val1 = Integer.parseInt(st.pop());
-                int val2 = Integer.parseInt(st.pop());
-                ans= val2/val1;
-                st.push(Integer.toString(ans));
+                int val1 = st.pop();
+                int val2 = st.pop();
+                st.push(val2/val1);
             }
             else{
-                st.push(s);
+                st.push(Integer.parseInt(s));
             }
 
         }
-    return ans;
+    return st.peek();
 
     }
 }
