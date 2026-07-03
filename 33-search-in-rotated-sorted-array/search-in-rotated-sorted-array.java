3a9eb34e32722736ec6404ec50897525496c9b20
Sparsh5001 <sparshbro@gmail.com>
2026-07-03 18:04:00 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (27.42%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -2,34 +2,28 @@ class Solution {
     public int search(int[] nums, int target) {
         int start = 0 ;
         int end = nums.length-1;
-        while(start<end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[mid]>nums[end]){
-                start=mid+1;
-            }else{
-                end=mid;
-            }
-        }
-        if(start==0) return binarySearch(nums,0,nums.length-1,target);
-        else{
-            int val = binarySearch(nums,0,start-1,target);
-            int val1 = binarySearch(nums,start,nums.length-1,target);
-            return Math.max(val,val1);
-        }
 
-    }
-    int binarySearch(int[] nums , int start , int end , int target){
         while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target)return mid;
-            if(nums[mid]>target){
-                end=mid-1;
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target) return mid;
+            if(nums[start] <= nums[mid]){
+                if(target>=nums[start] && target<nums[mid]){
+                    end=mid-1;
+                }
+                else{
+                    start=mid+1;
+                }
             }
-            else{
-                start=mid+1;
+            else if(nums[end] >= nums[mid]){
+                if(target<=nums[end] && target>nums[mid]){
+                    start=mid+1;
+                }else{
+                    end=mid-1;
+                }
             }
         }
         return -1;
+            
+
     }
 }
