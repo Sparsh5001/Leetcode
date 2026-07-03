510549bca8d2cbd224ca82f3a07d483f30a3e232
Sparsh5001 <sparshbro@gmail.com>
2026-07-04 00:18:01 +0530

    Time: 0 ms (100.00%) | Memory: 46.8 MB (21.88%) - LeetSync

---
+++ 1185-find-in-mountain-array/find-in-mountain-array.java
@@ -0,0 +1,71 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        int n = mountainArr.length();
+        int start = 1 ;
+        int end = n-2;
+        int mid = 0;
+        while(start<=end){
+            mid=start+(end-start)/2;
+            int val_mid = mountainArr.get(mid);
+            int val_prev = mountainArr.get(mid-1);
+            int val_next = mountainArr.get(mid+1);
+
+            if(val_mid>val_prev && val_mid>val_next){
+                break;
+            }
+            else{
+                if(val_prev>val_next){
+                    end=mid-1;
+                }else{
+                    start=mid+1;
+                }
+            }
+        }
+        int a = BinarySearch(target,0,mid,mountainArr,n,true);
+        int b = BinarySearch(target,mid+1,n-1,mountainArr,n,false);
+        //System.out.println(a);
+        //System.out.println(b);
+        if(a==-1){
+            return b;
+        }
+        if(b==-1){
+            return a;
+        }
+        return Math.min(a,b);
+    }
+
+    public int BinarySearch(int target , int start , int end , MountainArray mountainArr ,int n ,boolean asc){
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+            int val = mountainArr.get(mid);
+            if(val==target){
+                return mid;
+            }
+            if(val>target && asc){
+                end=mid-1;
+            }
+            else if(val>target && !asc){
+                start=mid+1;
+                //System.out.println(val);
+            }
+
+            if(val<target && asc){
+                start=mid+1;
+            }
+            else if(val<target && !asc){
+                end=mid-1;
+            }
+        }
+    return -1;
+    }
+}
