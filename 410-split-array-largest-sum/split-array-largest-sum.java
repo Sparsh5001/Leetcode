cbacdd1316eea0644cc7bf820b0991d4e866e331
Sparsh5001 <sparshbro@gmail.com>
2026-07-04 03:53:34 +0530

    Time: 2 ms (30.96%) | Memory: 42.8 MB (83.91%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -9,19 +9,14 @@ class Solution {
         while(start<=end){
             int mid = start+(end-start)/2;
             int sum = 0 ;
-            int groups = 1;
-            int i = 0 ;
             //checking how many groups it take to divide nums with set maximum limit
-            while(i<nums.length){
-                int w = nums[i];
-                if(sum + w > mid){
+            int groups = 1;
+            for (int num : nums) {
+                if (sum + num > mid) {
                     groups++;
-                    sum = w;
-                    i++;
-                }
-                else{
-                    sum = sum+w;
-                    i++;
+                    sum = num;
+                } else {
+                    sum += num;
                 }
             }
             //if exact or less groups used then reduce maximum to check if new lesser maximum is possible
