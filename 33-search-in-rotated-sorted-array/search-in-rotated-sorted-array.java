1de7bb1fa94360a3714793661cebb08eabed205c
Sparsh5001 <sparshbro@gmail.com>
2026-07-03 16:22:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (27.42%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,29 +1,52 @@
 class Solution {
     public int search(int[] nums, int target) {
-        return find(nums , target , 0 , nums.length-1);
+        
+
+        int start = 0 ;
+        int end = nums.length-1;
+
+        while(start<end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[mid]>nums[end]){
+                start=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+
+        if(start==0) return binarySearch(nums,0,nums.length-1,target);
+        else{
+            int val = binarySearch(nums,0,start-1,target);
+            int val1 = binarySearch(nums,start,nums.length-1,target);
+            return Math.max(val,val1);
+        }
+
     }
 
 
-    public int find(int[] nums , int target , int s , int e){
-        if (s > e) return -1;
-        int mid = s + (e-s)/2;
-        if(nums[mid] == target){
-            return mid;
-        }
-        if(nums[mid] >= nums[s]){
-            if(nums[s] <= target && target <= nums[mid]){
-                return find(nums , target , s , mid-1);
+
+
+    int binarySearch(int[] nums , int start , int end , int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target)return mid;
+            if(nums[mid]>target){
+                end=mid-1;
             }
             else{
-                return find(nums , target , mid+1 , e);
+                start=mid+1;
             }
         }
-        else {
-            if(nums[mid] <= target && target <= nums[e]){
-                return find(nums, target, mid+1, e);
-            } else {
-                return find(nums, target, s, mid-1);
-            }
-        }
+        return -1;
     }
+
+
+
+
+
+
+
+
+
 }
