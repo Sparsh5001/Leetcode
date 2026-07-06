bcff1d462da9dce5292797f10524b479e6077d54
Sparsh5001 <sparshbro@gmail.com>
2026-07-06 18:14:48 +0530

    Time: 1 ms (87.41%) | Memory: 45.4 MB (79.31%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,31 +1,38 @@
 class Solution {
+
     public List<List<Integer>> permute(int[] nums) {
-        
-        List<List<Integer>> result  = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        find(result , cur , nums , 0);
-        return result;
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
+        boolean[] used = new boolean[nums.length];
+
+        helper(nums, used, current, ans);
+
+        return ans;
     }
 
+    private void helper(int[] nums, boolean[] used, List<Integer> current,List<List<Integer>> ans) {
 
-    public void find(List<List<Integer>> result , List<Integer> cur , int[] nums , int index){
-
-        if(index == nums.length){
-            result.add(new ArrayList(cur));
+        if (current.size() == nums.length) {
+            ans.add(new ArrayList<>(current));
             return;
         }
 
-        for(int i = 0 ; i < cur.size() + 1 ; i++){
+        for (int i = 0; i < nums.length; i++) {
 
-            cur.add(i,nums[index]);
-            find(result , cur , nums , index+1);
-            cur.remove(i);
+            if (used[i]) {
+                continue;
+            }
 
+            // Choose and mark it as choosen
+            used[i] = true;
+            current.add(nums[i]);
 
+            // Explore now we check for remaing values and loop is used to iterate over boolean array
+            helper(nums, used, current, ans);
+
+            // Backtrack 1 element and make other combitions , untill we reach the first called loop
+            current.remove(current.size() - 1);
+            used[i] = false;
         }
-
-
-        return;
-
     }
 }
