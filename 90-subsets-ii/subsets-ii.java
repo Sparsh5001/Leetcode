155ba076ae958ad5258b27e72b14b44169c9dd2a
Sparsh5001 <sparshbro@gmail.com>
2026-07-06 17:17:24 +0530

    Time: 12 ms (5.55%) | Memory: 45.4 MB (10.53%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+
+        return helper(nums);
+    }
+
+    public List<List<Integer>> helper(int[] nums){
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+        buildSets(nums , 0 , ans , list);
+        return ans;
+    }
+
+    public void buildSets( int[]nums , int i , List<List<Integer>> ans , List<Integer> list){
+        if(i==nums.length){
+            if(ans.contains(list)){
+                return;
+            }
+            ans.add(new ArrayList<>(list)); 
+            return;
+        }
+        list.add(nums[i]);
+        buildSets(nums , i+1 , ans , list);
+       
+        list.removeLast();
+        buildSets(nums , i+1 , ans , list);
+
+    }
+}
