4f81641a9fa334de440dca08d28e39f1b7c33ec5
Sparsh5001 <sparshbro@gmail.com>
2026-07-19 21:56:19 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (68.50%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -12,26 +12,13 @@ class Solution {
     public ListNode reverseList(ListNode head) {
         ListNode temp = head;
         ListNode temp_fast = head;
+        ListNode tail = head;
         if(head==null || head.next==null){
-            return head;
-        }
-        if(head.next.next == null){
-            temp=head.next;
-            temp.next = head;
-            head.next = null;
-            head = temp;
             return head;
         }
 
         temp = head.next;
         temp_fast = head.next.next;
-
-
-        temp.next=head;
-        head.next=null;
-        head = temp;
-        temp = temp_fast;
-        temp_fast = temp_fast.next;
 
         while(temp_fast!=null){
             temp.next=head;
@@ -41,6 +28,7 @@ class Solution {
         }
         temp.next = head;
         head = temp;
+        tail.next = null;
         return head;
     }
 }
