b6ac0ab2ca1009330cd41ee81d63be4401b9c574
Sparsh5001 <sparshbro@gmail.com>
2026-07-20 16:26:53 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (72.89%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -11,14 +11,11 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-
         if(head==null || head.next == null){
             return false;
         }
-
         ListNode slow = head;
         ListNode fast = head.next;
-
         while(fast!=null && fast.next!=null){
             if(slow == fast){
                 return true;
