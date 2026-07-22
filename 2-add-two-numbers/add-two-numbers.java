724cd3ed09a466a0b7b45fd3b91af3bd92b3c66d
Sparsh5001 <sparshbro@gmail.com>
2026-07-22 21:34:55 +0530

    Time: 1 ms (100.00%) | Memory: 46.5 MB (57.62%) - LeetSync

---
+++ 2-add-two-numbers/add-two-numbers.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        if (length(l2) > length(l1)) {
+            ListNode temp = l1;
+            l1 = l2;
+            l2 = temp;
+        }
+
+        ListNode temp1 = l1 ;
+        ListNode temp2 = l2 ;
+        ListNode prev = null;
+        int carry = 0;
+
+        while(temp1!=null && temp2!=null){
+
+            int sum = temp1.val + temp2.val + carry;
+            temp1.val = sum % 10;
+            carry = sum / 10;
+
+            prev = temp1;
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+
+        while(temp1!=null){
+            int sum = temp1.val + carry;
+            temp1.val = sum % 10;
+            carry = sum / 10;
+
+            prev = temp1;
+            temp1 = temp1.next;
+        }
+        
+        if(carry == 1){
+            prev.next = new ListNode(1);
+        }
+        return l1;
+    }
+
+
+    public ListNode rev(ListNode head){
+        ListNode current = head;
+        ListNode prev = null;
+        while(current!=null){
+            ListNode next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;  
+        }
+        return prev;
+    }
+
+    public int length(ListNode head) {
+        int len = 0;
+        while (head != null) {
+            len++;
+            head = head.next;
+        }
+        return len;
+    }
+
+}
