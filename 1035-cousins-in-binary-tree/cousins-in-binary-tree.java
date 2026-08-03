1617fc4c6d64060d851bbae5a436fad8a4db59d8
Sparsh5001 <sparshbro@gmail.com>
2026-08-04 01:15:25 +0530

    Time: 1 ms (27.34%) | Memory: 43.1 MB (59.01%) - LeetSync

---
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -0,0 +1,72 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isCousins(TreeNode root, int x, int y) {
+
+        if (root == null) {
+            return false;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int curLevel = 0;
+
+        int xLevel = 0;
+        int yLevel = 0;
+        TreeNode xParent = null;
+        TreeNode yParent = null;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+
+                TreeNode current = queue.poll();
+                
+                if (current.left != null) {
+                    if(current.left.val==x){
+                        xLevel=curLevel+1;
+                        xParent = current;
+                    }
+                    if(current.left.val==y){
+                        yLevel=curLevel+1;
+                        yParent = current;
+                    }
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    if(current.right.val==x){
+                        xLevel=curLevel+1;
+                        xParent = current;
+                    }
+                    if(current.right.val==y){
+                        yLevel=curLevel+1;
+                        yParent = current;
+                    }
+                    queue.offer(current.right);
+                }
+            }
+            curLevel++;
+        }
+
+        if(yLevel==xLevel && yParent!=xParent){
+            return true;
+        }
+
+        return false;
+    }
+}
